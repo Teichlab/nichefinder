eb171472581eb8f082326dca832c9e4c0e5bb404
Jan Patrick Pett <jpatrickpett@googlemail.com>
2024-12-03 23:29:43 +0000

    add setup.py

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # iss2niche
 
 A collection of functions for working with ISS data, including label transfer from a suspension reference and niche analysis.
+
+## Installation
+
+Optional: create a new conda environment with Python
+
+```bash
+mamba create -n iss2niche "python<3.12"
+mamba activate iss2niche
+```
+
+### Install from GitHub
+
+```bash
+pip install git+ssh://git@github.com/Teichlab/iss2niche.git
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iss2niche",
+    version="0.1.0",
+    description="A project for niche analysis using spatial and suspension datasets",
+    author="J.P.Pett",
+    author_email="jp30@sanger.ac.uk",
+    url="https://github.com/Teichlab/snp2cell",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scanpy",
+        "scipy",
+        "scikit-learn",
+        "matplotlib",
+        "pandas",
+        "joblib",
+        "networkx",
+    ],
+    extras_require={"dev": ["pytest", "flake8", "black"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5, <3.12",
+)
